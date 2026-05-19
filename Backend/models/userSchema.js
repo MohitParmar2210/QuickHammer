56230216cb960fb634740155e9ad6ca32440b23e
Mohit Parmar <mohitparmar2210@gmail.com>
2026-05-19 17:02:27 +0530

    Backend fixes

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -10,7 +10,7 @@ const userSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    selected: false,
+    select: false,
     minLength: [8, "Password must caontain at least 8 characters."],
   },
   email: String,
--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,69 @@
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./config/config.env" });
+
+// import { connection } from "./databse/connection.js";
 // import app from "./app.js";
 // import cloudinary from "cloudinary";
 
+// import { User } from "./models/userSchema.js";
 
+// connection();
 
+// // Create Admin
+// const createAdmin = async () => {
+//   const existingAdmin = await User.findOne({
+//     email: "mohitparmar2210@gmail.com",
+//   });
+
+//   if (existingAdmin) {
+//     console.log("Admin already exists");
+//     return;
+//   }
+
+//   await User.create({
+//     userName: "Admin",
+//     email: "mohitparmar2210@gmail.com",
+//     password: "admin123",
+//     role: "Super Admin",
+//     phone: "9131797927",
+//     address: "India",
+//     profileImage: {
+//       public_id: "dummy",
+//       url: "https://dummyimage.com/200x200",
+//     },
+//   });
+
+//   console.log("Admin Created");
+// };
+
+// createAdmin();
+
+// // Cloudinary config
 // cloudinary.v2.config({
-//       cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-//       api_key: process.env.CLOUDINARY_API_KEY,
-//       api_secret: process.env.CLOUDINARY_API_SECRET
-
+//   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+//   api_key: process.env.CLOUDINARY_API_KEY,
+//   api_secret: process.env.CLOUDINARY_API_SECRET,
 // });
 
-// app.listen(process.env.PORT,()=>{
-//       console.log(`Sever listening on port ${process.env.PORT}`);
+// // Start Server
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server listening on port ${process.env.PORT}`);
 // });
-
 
 import dotenv from "dotenv";
 dotenv.config({ path: "./config/config.env" });
+
 import { connection } from "./databse/connection.js";
-
-
-//dotenv.config({ path: "./.env" }); // or "./config/config.env" if you keep it there
-// dotenv.config({ path: "./config/config.env" });
-// Now import everything else
 import app from "./app.js";
 import cloudinary from "cloudinary";
 
 connection();
-//  Cloudinary config
+
+// Cloudinary config
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Start Server
